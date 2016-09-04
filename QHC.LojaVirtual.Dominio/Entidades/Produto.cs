d81764167e1d18af4103d8562c5bfab8e4523135
davidsou <davidsou@gmail.com>
2016-09-04 19:43:46 -0300

    nova classe adicionada

---
+++ QHC.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QHC.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+    
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Descricao {get;set;}
+    }
+}
