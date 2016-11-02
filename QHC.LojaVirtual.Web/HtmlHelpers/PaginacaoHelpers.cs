bb0893e381370fed6045efffae454ea43337c865
davidsou <davidsou@gmail.com>
2016-11-02 12:25:57 -0200

    Aula 8

---
+++ QHC.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,36 @@
+﻿using QHC.LojaVirtual.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QHC.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        //total paginas = 3
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaURL)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaURL(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
---
+++ QHC.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QHC.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+        public int ItensPorPagina { get; set; }
+        public int PaginaAtual { get; set; }
+        //Propriedade não necessita de set.
+        //Ceiling arrendonda para um número inteiro.
+        public int TotalPagina
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal) ItensTotal/ItensPorPagina);;
+            }
+        }
+    }
+}
