a028f952fc20da9ef1b6232aab3f32a7ff7b6210
davidsou <davidsou@gmail.com>
2016-11-03 08:21:43 -0200

    Aula 11

--- QHC.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ QHC.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace QHC.LojaVirtual.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:null,
+                url:"Pagina{pagina}",
+                defaults:new{controller="Vitrine",action = "ListaProdutos"}
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
