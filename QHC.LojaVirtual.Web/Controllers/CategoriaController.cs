35ba017ff0055ba0d6f20a75559e4fd88f0ce912
davidsou <davidsou@gmail.com>
2017-01-19 06:12:18 -0200

    Including untracked files on project

---
+++ QHC.LojaVirtual.Web/Controllers/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using QHC.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QHC.LojaVirtual.Web.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        // GET: Categoria
+        public PartialViewResult Menu(string categoria = null)
+        {
+            ViewBag.CategoriaSelecionada = categoria;
+            _repositorio = new ProdutosRepositorio();
+
+            IEnumerable<string> categorias = _repositorio.Produtos
+                .Select(c => c.Categoria.TrimEnd())
+                .Distinct()
+                .OrderBy(c => c);           
+
+            return PartialView(categorias);
+        }
+    }
+}
---
+++ QHC.LojaVirtual.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using QHC.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QHC.LojaVirtual.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        // GET: Produto 
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+            return View(produtos);
+        }
+    }
+}
