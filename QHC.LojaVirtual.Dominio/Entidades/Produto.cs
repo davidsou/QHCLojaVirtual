c575742d1334b45e014d9faba9d6c244348b4cd8
David Oliveira Soares <davidsou@hotmail.com>
2017-01-27 21:03:49 -0200

    Aula 33 parcial

--- QHC.LojaVirtual.Dominio/Entidades/Produto.cs
+++ QHC.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -12,10 +12,19 @@ namespace QHC.LojaVirtual.Dominio.Entidades
     {
         [HiddenInput(DisplayValue= false)]
         public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage= "Digite o nome do produto")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite a descrição")]
         [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
+
+         [Required(ErrorMessage= "Digite o valor")]
+        [Range(0.01, double.MaxValue, ErrorMessage="Valor inválido")]
         public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Digite a Categoria")]
         public string Categoria { get; set; }
     }
 }
