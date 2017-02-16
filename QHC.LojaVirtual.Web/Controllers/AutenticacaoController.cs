e6b29e74ce04c021897fcde688e4010b8c2d4f97
davidsou <davidsou@gmail.com>
2017-02-16 00:53:47 -0200

    Aula 40

--- QHC.LojaVirtual.Web/Controllers/AutenticacaoController.cs
+++ QHC.LojaVirtual.Web/Controllers/AutenticacaoController.cs
@@ -5,23 +5,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace QHC.LojaVirtual.Web.Controllers
 {
     public class AutenticacaoController : Controller
     {
+        AdministradoresRepositorio _repositorio;
         // GET: Autenticacao
-        public ActionResult Index()
+        public ActionResult Login(string returnUrl)
         {
-            return View();
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new Administrador());
         }
-        public  void Login ()
+        public ActionResult Login(Administrador administrador, string returnUrl)
         {
-            
-            Administrador administrador = new Administrador();
-            administrador.Login = "QHC";
-            AdministradoresRepositorio repositorio = new AdministradoresRepositorio();
-            var admin = repositorio.ObterAdministrador(administrador);
+            _repositorio = new AdministradoresRepositorio();
+
+            if (ModelState.IsValid)
+            {
+                Administrador admin = _repositorio.ObterAdministrador(administrador);
+                if (admin != null)
+                {
+                    if (!Equals(administrador.Login, admin.Senha))
+                    {
+                        ModelState.AddModelError("", "Senha não confere");
+                        
+
+                    }
+                    else
+                    {
+                        FormsAuthentication.SetAuthCookie(admin.Login, false);
+
+                        if (Url.IsLocalUrl(returnUrl)
+                            && returnUrl.Length > 1
+                            && returnUrl.StartsWith("/")
+                            && !returnUrl.StartsWith("//")
+                            && !returnUrl.StartsWith("/\\"))
+
+                            return Redirect(returnUrl);
+                    }
+                }
+                {
+                    ModelState.AddModelError("", "Administrador não localizado");
+                }
+            }
+
+            //  
 
         }
     }
