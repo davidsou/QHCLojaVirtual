6ee122ad3dc469f347bd47d56c03c7d37f01ae7b
David Oliveira Soares <davidsou@hotmail.com>
2017-03-14 09:26:19 -0300

    Inclusão de projeto SQL.

---
+++ QHC.LojaVirtual.Dominio/Entidades/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QHC.LojaVirtual.Dominio.Entidades
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public int CategoriaCodigo { get; set; }
+        public string CategoriaDescricao { get; set; }
+    }
+}
